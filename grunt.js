8db1953403079b55ca8389e6495eb5aa972a9569
eug <eug@lubuntu-64.(none)>
2012-11-26 22:54:30 +1000

    Added an option to customise the dirty marker, so that it can be set to '+'. Also made the target prop configurable in grunt.js.

--- grunt.js
+++ grunt.js
@@ -3,8 +3,10 @@ module.exports = function( grunt ) {
 	grunt.initConfig({
 		lint: {
 			files: [ "grunt.js", "tasks/**/*.js" ]
-		}
+		},
+		describe: { dirtyMarker: '+', prop: 'meta.git.version', },
 	});
 
 	grunt.registerTask( "default", "lint" );
+	grunt.loadTasks("tasks/");
 };
