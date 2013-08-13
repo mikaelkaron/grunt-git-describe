310bfa79568c9fbefaf7211dcd6870750d0ab05d
Mikael Karon <mikael@karon.se>
2013-08-13 17:20:31 +0800

    Added support for callback.
    Added support for commitish. Closes mikaelkaron/grunt-git-describe#9
    Added support for templating. Closes mikaelkaron/grunt-git-describe#8

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # grunt-git-describe
 
-> Describes current git commit
+> Describes git commit
 
 ## Getting Started
 This plugin requires Grunt `~0.4.0`
@@ -38,26 +38,42 @@ grunt.initConfig({
 ### Options
 
 #### options.prop
-Type: `String`
+Type: `String`  
 Default value: `''`
 
 A string value that is used as a property name for storing the result of this task
 
+#### options.callback
+Type: `Function`  
+Default value: `undefined`
+
+
+A function that will be called with the result of this task
+
 #### options.cwd
-Type: `String`
+Type: `String`  
 Default value: `'.'`
 
 A string value that is used to do set the current working directory when spawning the `git` command
 
-#### options.dirtyMark
-Type: `String`
-Default value: `'-dirty'`
+#### options.commitish
+Type: `String`  
+Default value: `undefined`
 
-A string value that is used as the for the `dirty=` option passed to `git`
+A string value that is used as `commitish` for `git`. Default is to use `HEAD`.
+
+### options.template
+Type: `String`  
+Default value: `{%=tag%}-{%=since%}-{%=object%}{%=dirty%}`
+
+A string value used to format the result of this task
 
 ## Contributing
+
 In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
 
 ## Release History
-1.0 - First release, compatible with grunt `~0.3.0`
-2.0 - Updated version for grunt `~0.4.0`
+
+2.1.0 - Added support for `callback` and `template` and deprecated `dirtyMark`  
+2.0.0 - Updated version for grunt `~0.4.0`  
+1.0.0 - First release, compatible with grunt `~0.3.0`
--- tasks/git-describe.js
+++ tasks/git-describe.js
@@ -9,26 +9,43 @@
 "use strict";
 
 module.exports = function (grunt) {
-	var CWD = "cwd";
 	var PROP = "prop";
-	var DIRTY_MARK = "dirtyMark";
+	var CALLBACK = "callback";
+	var CWD = "cwd";
+	var COMMITISH = "commitish";
+	var TEMPLATE = "template";
+	var RE =/(?:(.*)-(\d+)-g)?([a-fA-F0-9]{7})(-dirty)?$/;
 
-	grunt.registerMultiTask("git-describe", "Describes current git commit", function (prop, cwd) {
+	var OPTIONS = {};
+	OPTIONS[CWD] = ".";
+	OPTIONS[TEMPLATE] = "{%=tag%}-{%=since%}-{%=object%}{%=dirty%}";
+
+	// Register additional delimiters
+	grunt.template.addDelimiters("git-describe", "{%", "%}");
+
+	grunt.registerMultiTask("git-describe", "Describes git commit", function (cwd, commitish, template) {
 		// Start async task
 		var done = this.async();
 
-		// Define default options
-		var options = {};
-		options[CWD] = ".";
-		options[DIRTY_MARK] = "-dirty";
+		// Store some locals
+		var name = this.name;
+		var target = this.target;
+		var args = this.args;
 
-		// Load cli options (with defaults)
-		options = this.options(options);
+		// Get task options
+		var options = this.options(OPTIONS);
 
-		// Override options
-		options[PROP] = prop || options[PROP];
-		options[CWD] = cwd || options[CWD];
-		options[DIRTY_MARK] = grunt.option(DIRTY_MARK) || options[DIRTY_MARK];
+		// Update `options` with cli values
+		[ CWD, COMMITISH, TEMPLATE ].forEach(function (key, index) {
+			options[key] = [
+				args[index],
+				grunt.option([ name, target, key ].join(".")),
+				grunt.option([ name, key ].join(".")),
+				grunt.option(key)
+			].filter(function (value) {
+				return grunt.util.kindOf(value) !== "undefined";
+			})[0] || options[key];
+		});
 
 		// Log flags (if verbose)
 		grunt.log.verbose.writeflags(options);
@@ -36,30 +53,108 @@ module.exports = function (grunt) {
 		// Spawn git
 		grunt.util.spawn({
 			"cmd" : "git",
-			"args" : [ "describe", "--tags", "--always", "--long", "--dirty=" + options[DIRTY_MARK] ],
+			"args" : [ "describe", "--tags", "--always", "--long", options[COMMITISH] || "--dirty" ],
 			"opts" : {
 				"cwd" : options[CWD]
 			}
 		}, function (err, result) {
 			// If an error occurred...
 			if (err) {
-				// Done with false
-				done(false);
-
 				// Fail with error
 				grunt.fail.warn(err);
+
+				// Done with false
+				done(false);
 			}
 
-			// Output
-			grunt.log.ok(result);
+			// Get matches
+			var matches = result.toString().match(RE);
+
+			// Make sure we matched
+			if (matches === null) {
+				// Fail with error
+				grunt.fail.warn("Unable to match '" + result + "'");
+
+				// Done with false
+				done(false);
+			}
+
+			// Define extended properties on `matches`
+			Object.defineProperties(matches, {
+				"toString" : {
+					"enumerable" : true,
+					"value" : function(override) {
+						var me = this;
+
+						return grunt.template.process(override || me[TEMPLATE], {
+							"data": me,
+							"delimiters": "git-describe"
+						});
+					}
+				},
+
+				"template" : {
+					"enumerable": true,
+					"value" : options[TEMPLATE],
+					"writable" : true
+				},
+
+				"tag" : {
+					"enumerable" : true,
+					"get" : function () {
+						return this[1];
+					},
+					"set" : function (value) {
+						this[1] = value;
+					}
+				},
+
+				"since" : {
+					"enumerable" : true,
+					"get" : function () {
+						return this[2];
+					},
+					"set" : function (value) {
+						this[2] = value;
+					}
+				},
+
+				"object" : {
+					"enumerable" : true,
+					"get" : function () {
+						return this[3];
+					},
+					"set" : function (value) {
+						this[3] = value;
+					}
+				},
+
+				"dirty" : {
+					"enumerable" : true,
+					"get" : function () {
+						return this[4];
+					},
+					"set" : function (value) {
+						this[4] = value;
+					}
+				}
+			});
+
+			// Log
+			grunt.log.ok(matches);
 
 			// If we were passed a prop we should update
 			if (options[PROP]) {
-				grunt.config(options[PROP], result);
+				grunt.config(options[PROP], matches);
+			}
+
+			// If we were passed a callback we should call
+			if (grunt.util.kindOf(options[CALLBACK]) === "function") {
+				callback.call(null, matches);
 			}
 
 			// Done with result
-			done(result);
+			done(matches);
 		});
 	});
 };
